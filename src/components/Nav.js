2b67cde91ff43b2eb810bb500b32d4e4585afbc5
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-10-22 09:40:50 -0400

    Working on classes to create css global and component level

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
+import classes from "../styles/nav.module.css";
 
 const Nav = () => {
   return (
-    <nav>
-      <Link to="/">Joshua Aldrich</Link>
-      <Link to="/about">About Me</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/resume">Resume</Link>
+    <nav className={classes.nav}>
+      <Link className={classes.link} to="/">
+        Joshua Aldrich
+      </Link>
+      <Link className={classes.link} to="/about">
+        About Me
+      </Link>
+      <Link className={classes.link} to="/projects">
+        Projects
+      </Link>
+      <Link className={classes.link} to="/contact">
+        Contact
+      </Link>
+      <Link className={classes.link} to="/resume">
+        Resume
+      </Link>
     </nav>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import "./style.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
