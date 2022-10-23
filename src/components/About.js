335a4e414de65b39358b317ffb9021f11c95ac5d
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-10-23 08:20:34 -0400

    about me information filled out

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,22 @@
 import classes from "../styles/about.module.css";
+import profilePic from "../images/ProfilePicture.jpg";
 
 const About = () => {
-  return <h2>About Me</h2>;
+  return (
+    <div className={classes.about}>
+      <div>
+        <img src={profilePic} alt="Hello, I'm Josh" />
+      </div>
+      <p>
+        Hello my name is Joshua Aldrich. I have had a passion for coding ever
+        since I started my college degree. I ended up taking a break to focus on
+        working for a couple years but decided to get back into it once I saved
+        enough to join the UCF coding bootcamp. I have constantly watched videos
+        on youtube and the passion for coding hasn't left so I'm glad to take
+        this opportunity to make coding a career path.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import classes from "../styles/projects.module.css";
 
 function Projects() {
-  return <div>Projects</div>;
+  return <div>{[<p>1</p>, <p>2</p>]}</div>;
 }
 
 export default Projects;
