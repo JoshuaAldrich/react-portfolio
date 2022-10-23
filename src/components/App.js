a6b4c2a41d0fc34821b1adb6df91ab4eda3547e7
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-10-23 09:31:03 -0400

    Added more component elements

--- src/components/App.js
+++ src/components/App.js
@@ -11,13 +11,44 @@ const App = () => {
     <>
       <Nav />
 
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      <div className="pageContainer">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+      <article id="contact" class="contact-me">
+        <div class="container">
+          <h2>Contact Me</h2>
+
+          <div class="contactme">
+            <p>
+              <a href="tel:407.670.4797">407-670-4797</a>
+            </p>
+            <p>
+              <a href="mailto:joshuataldrich@gmail.com">
+                joshuataldrich@gmail.com
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/JoshuaAldrich" target="blank">
+                Github
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://www.linkedin.com/in/joshua-aldrich-4924b323b/"
+                target="blank"
+              >
+                LinkedIn
+              </a>
+            </p>
+          </div>
+        </div>
+      </article>
     </>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,18 @@
 import classes from "../styles/contact.module.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <form className={classes.form}>
+        <label>Name</label>
+        <input />
+        <label>Email Address:</label>
+        <input />
+        <label>Message</label>
+        <textarea rows={10}></textarea>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
